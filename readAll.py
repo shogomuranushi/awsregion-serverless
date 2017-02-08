6af4b10be2d7749bdb5c9b32973eebd618dd536c
shogo muranushi <muranushi@cloudpack.jp>
2017-02-08 10:28:11 +0900

    update v0.9.4

--- readAll.py
+++ readAll.py
@@ -1,15 +1,28 @@
+#from prettyprint import pp
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table    = dynamodb.Table('regions')
 
 def readAll(event, context):
-    body = u"list"
+    try:
+	data = table.scan()
+	print(data)
+	body = data
 
-    response = {
-        "statusCode": 200,
-        "body": body
-    }
+        response = {
+            "statusCode": 200,
+            "body": body
+        }
 
-    return response
+        return response
+
+    except Exception as e:
+        return e
 
 if __name__ == '__main__':
-   result = readAll(None, None)
-   print(result)
+    result = readAll(None, None)
+    print(result)
